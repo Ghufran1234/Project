28bfe4e5401cc1b3324355f7b0795aa51e092068
Ghufy123 <Ghufranz123@gmail.com>
2018-11-07 17:04:50 +0000

    Adding a layout

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,30 @@
+
+<DOCTYPE html>
+<html>
+<head>
+    <meta charset="uft-8">
+
+    <title>Futy</title>
+
+
+</head>
+
+<body>
+
+@yield('content')
+
+
+@section('sidebar')
+    <div class = "sidebar">
+        <h3>Sidebar</h3>
+        This is the sidebar
+
+    </div>
+</body>
+
+
+
+
+</html>
+
+</DOCTYPE>
